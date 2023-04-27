bfedbc7dbf9b84fdc727adf2a857610ed7d483a8
HemalDesai <hemald332@gmail.com>
2023-04-27 13:10:12 +0530

    final build

--- LNMservices/urls.py
+++ LNMservices/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('LNMfood/', include('LNMfood.urls')),
+    path('LNMtaxi/', include('LNMtaxi.urls')),
 
 
 ]
